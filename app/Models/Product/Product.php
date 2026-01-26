7cc72c8a31b9c77c45b41326049eeb07c6cf0d37
William Luisan <williamluisan@gmail.com>
2026-01-26 02:54:08 +0000

    chore: update product model to store public id

--- app/Models/Product/Product.php
+++ app/Models/Product/Product.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    protected $fillable = ['name', 'code', 'price', 'stock'];
+    protected $fillable = ['name', 'code', 'price', 'stock', 'public_id'];
 }
