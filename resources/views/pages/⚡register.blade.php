601def11ace8d5dae9a13b1e0d446f5c48fd6cda
William Luisan <williamluisan@gmail.com>
2026-07-24 23:01:46 +0700

    chore: test tailwind

--- resources/views/pages/⚡register.blade.php
+++ resources/views/pages/⚡register.blade.php
@@ -24,16 +24,18 @@ new class extends Component
 };
 ?>
 
-<div>
-    <div class="mb-6">
-        <flux:heading>Create an account</flux:heading>
-        <flux:text class="mt-2">We're excited to have you on board.</flux:text>
+<div class="flex items-center justify-center min-h-screen">
+    <form wire:submit="createAccount" class="w-full max-w-md">
+        <div class="mb-6 text-center">
+            <flux:heading>Create an account</flux:heading>
+            <flux:text class="mt-2">We're excited to have you on board.</flux:text>
+        </div>
+        <flux:input label="Name" wire:model="name"/>
+        <flux:input class="mb-6" label="Email" wire:model="email" />
+        <div class="mb-6 flex *:w-1/2 gap-4">
+            <flux:input label="Password" wire:model="password" />
+            <flux:input label="Confirm password" wire:model="password_confirmation" />
+        </div>
+        <flux:button type="submit" variant="primary" class="w-full">Create account</flux:button>
     </div>
-    <flux:input label="Name" wire:model="name"/>
-    <flux:input class="mb-6" label="Email" wire:model="email" />
-    <div class="mb-6 flex *:w-1/2 gap-4">
-        <flux:input label="Password" wire:model="password" />
-        <flux:input label="Confirm password" wire:model="password_confirmation" />
-    </div>
-    <flux:button wire:click="createAccount" variant="primary">Create account</flux:button>
 </div>
